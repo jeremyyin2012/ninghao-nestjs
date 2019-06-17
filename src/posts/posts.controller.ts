aea88561486cbc71a1b7e0b69316ad72ef68bd72
Jeremy Yin <jeremyyin2012@gmail.com>
2019-06-17 23:19:36 +0800

    post请求，发body json格式数据，通过body拿到数据，状态响应是201，跟tornado全部200有些不一样，更接近restful标准

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Query, Headers, Param } from '@nestjs/common';
+import { Controller, Get, Post, Req, Query, Headers, Param, Body } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
@@ -18,5 +18,11 @@ export class PostsController {
         return {
             title: `Post ${params.id}`
         }
+    }
+
+    @Post()
+    store(@Body() body) {
+        console.log(body)
+        return body
     }
 }
