0e349316f85a26ca772bfda0df9397ce69ada67b
Jeremy Yin <jeremyyin2012@gmail.com>
2019-06-18 22:24:11 +0800

    然后把异常定义一下

--- src/core/filters/demo.filter.ts
+++ src/core/filters/demo.filter.ts
@@ -1,6 +1,16 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 
-@Catch()
+@Catch(HttpException)
 export class DemoFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      path: request.url,
+    });
+  }
 }
