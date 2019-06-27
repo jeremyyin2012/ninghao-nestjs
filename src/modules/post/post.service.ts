607d3d86be4fc41c5f1f15d30a290ed7d29c7cea
Jeremy Yin <jeremyyin2012@gmail.com>
2019-06-27 22:22:50 +0800

    现在可以添加实体数据记录

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>,
+    ) { }
+
+    async store(data) {
+        const entity = await this.postRepository.create(data);
+        return await this.postRepository.save(entity);
+    }
+}
