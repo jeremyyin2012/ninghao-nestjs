8eda01f86ee94b5862598e7912f3e7cb2c545368
Jeremy Yin <jeremyyin2012@gmail.com>
2019-06-29 16:49:28 +0800

    创建用户；请求单个用户；密码字段排除；密码加密；修改密码；

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { UserDto, UpdatePasswordDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
+
+    async store(data: UserDto) {
+        const { username } = data;
+        const user = await this.userRepository.findOne({ username });
+        if (user) {
+            throw new BadRequestException('用户已经存在');
+        }
+        const entity = await this.userRepository.create(data);
+        await this.userRepository.save(entity);
+        return entity;
+    }
+
+    async show(id: string) {
+        const entity = await this.userRepository.findOne(id);
+        if (!entity) {
+            throw new NotFoundException('没有找到用户');
+        }
+        return entity;
+    }
+
+    async updatePassword(id: string, data: UpdatePasswordDto) {
+        const { password, newPassword } = data;
+        const entity = await this.userRepository.findOne(id)
+        if (!entity) {
+            throw new NotFoundException('没有找到用户')
+        }
+        const match = await entity.comparePassword(password)
+        if (!match) {
+            throw new BadRequestException('密码错误')
+        }
+        entity.password = newPassword
+        return await this.userRepository.save(entity)
+    }
+}
